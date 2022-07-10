57fa97c098691c60ab229259277ac8806f80ab47
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-07-11 00:02:36 +0200

    facts + team page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import { IconContext } from 'react-icons';
+import { FaHatCowboy } from 'react-icons/fa';
+import { GoTools } from 'react-icons/go';
+import { IoMdPlanet } from 'react-icons/io';
+
 function App() {
 
     function landingScroll(): void {
@@ -52,7 +57,7 @@ function App() {
                 </div>
 
                 <div className='flex flex-row justify-center items-center h-[500px] mt-12'>
-                    <div className='border border-gray-200 bg-white rounded-md drop-shadow-2xl h-72 w-[325px] mx-6'>
+                    <div className='border border-gray-200 bg-white rounded-md drop-shadow-2xl h-72 w-[325px] mx-6 relative'>
                         <h2 className='text-2xl text-center font-bold text-black mt-12 mb-6'>WE ARE COWBOYS</h2>
                         <p className='text-gray-700 text-center'>
                             You might have guessed that from our <br/>
@@ -61,8 +66,13 @@ function App() {
                             who'd like to go to space. <br/>
                             Deal with it.
                         </p>
+                        <div className='rounded-full w-16 h-16 border border-black bg-yellow-400 flex flex-row items-center justify-center absolute top-[-32px] right-[130px]'>
+                            <IconContext.Provider value={{className: 'text-gray-700 w-8 h-8'}}>
+                                <FaHatCowboy></FaHatCowboy>
+                            </IconContext.Provider>
+                        </div>
                     </div>
-                    <div className='border border-gray-200 bg-white rounded-md drop-shadow-2xl h-72 w-[325px] mx-6'>
+                    <div className='border border-gray-200 bg-white rounded-md drop-shadow-2xl h-72 w-[325px] mx-6 relative'>
                         <h2 className='text-2xl text-center font-bold text-black mt-12 mb-6'>WE LIKE TO TINKER</h2>
                         <p className='text-gray-700 text-center'>
                             There are a lot of things lying around on <br/>
@@ -70,8 +80,13 @@ function App() {
                             putting them together, and got really <br/>
                             good at rocket building.
                         </p>
+                        <div className='rounded-full w-16 h-16 border border-black bg-yellow-400 flex flex-row items-center justify-center absolute top-[-32px] right-[130px]'>
+                            <IconContext.Provider value={{className: 'text-gray-700 w-8 h-8'}}>
+                                <GoTools></GoTools>
+                            </IconContext.Provider>
+                        </div>
                     </div>
-                    <div className='border border-gray-200 bg-white rounded-md drop-shadow-2xl h-72 w-[325px] mx-6'>
+                    <div className='border border-gray-200 bg-white rounded-md drop-shadow-2xl h-72 w-[325px] mx-6 relative'>
                         <h2 className='text-2xl text-center font-bold text-black mt-12 mb-6'>WE EXPLORE SPACE</h2>
                         <p className='text-gray-700 text-center'>
                             Once we got out first proper rocket, <br/>
@@ -80,14 +95,42 @@ function App() {
                             the world. <br/>
                             Who would have guessed?
                         </p>
+                        <div className='rounded-full w-16 h-16 border border-black bg-yellow-400 flex flex-row items-center justify-center absolute top-[-32px] right-[130px]'>
+                            <IconContext.Provider value={{className: 'text-gray-700 w-8 h-8'}}>
+                                <IoMdPlanet></IoMdPlanet>
+                            </IconContext.Provider>
+                        </div>
                     </div>
                 </div>
 
             </section>
 
-            
-            <section className='bg-green-300 h-screen'>
-
+            {/* TEAM PAGE */}
+            <section className='bg-blue-300 h-screen'>
+                <div className='flex flex-col items-center'>
+                    <h2 className='text-3xl font-bold text-white mt-12'>THE TEAM</h2>
+                    <p className='text-gray-100 text-center mt-4'>
+                        Yep, that's us. Six fine fellows, eager to get on the next adventure. <br/>
+                        Click on the pictures to get some more information!
+                    </p>
+                </div>
+                <div className='grid grid-cols-12 mx-56 justify-items-center mt-12'>
+                    <div className='col-span-4 border'>
+                        Col
+                    </div>
+                    <div className='col-span-4 border'>
+                        Col
+                    </div>
+                    <div className='col-span-4 border'>
+                        Col
+                    </div>
+                    <div className='col-span-6 border'>
+                        Col
+                    </div>
+                    <div className='col-span-6 border'>
+                        Col
+                    </div>
+                </div>
             </section>
 
             <section className='bg-pink-300 h-screen'>
